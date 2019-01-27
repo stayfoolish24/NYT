f9c70ff81de8baaa03fd385415ee74f10a00e01f
SeoulStore <stayfoolish24@gmail.com>
2019-01-27 22:39:33 +0900

    first

---
+++ src/components/Title.js
@@ -0,0 +1,6 @@
+import React, { PropTypes } from 'react'
+import { StyleSheet, Text } from 'react-native'
+import AppText from './AppText'
+import * as globalStyles from '../styles/global'
+
+const Title = ({ style, children }) => <AppText />
