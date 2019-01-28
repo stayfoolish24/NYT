20b1035c170e0957d7daec5690b64b33f754835b
SeoulStore <stayfoolish24@gmail.com>
2019-01-28 23:37:07 +0900

    text

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,4 +3,22 @@ import { StyleSheet, Text } from 'react-native'
 import AppText from './AppText'
 import * as globalStyles from '../styles/global'
 
-const Title = ({ style, children }) => <AppText />
+const Title = ({ style, children }) => (
+  <AppText style={[styles.title, style]}>{children}</AppText>
+)
+
+Title.propTypes = {
+  style: Text.propTypes.style,
+  children: PropTypes.node
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: 'HelveticaNeue-CondenseBold',
+    fontSize: 18,
+    color: globalStyles.HEADER_TEXT_COLOR,
+    backgroundColor: `${globalStyles.BG_COLOR}99`
+  }
+})
+
+export default Title
