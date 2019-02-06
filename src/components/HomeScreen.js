7b4e3afc108dd741db668afa9aa59334dc144d82
stayfoolish24 <stayfoolish24@gmail.com>
2019-02-06 22:24:08 +0900

    Search 추가

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import { TabBarIOS, Text } from 'react-native'
 import NewsFeed from './NewsFeed'
+import Search from './Search'
 import * as globalStyles from '../styles/global'
+
+//iOS에서 상태바의 내용을 흰색으로
+statusbar.setBarStyle('light-content')
 
 class HomeScreen extends Component {
   constructor(props) {
@@ -11,9 +15,21 @@ class HomeScreen extends Component {
     }
   }
 
+  showBookmarkAlert() {
+    Vibration.vibrate()
+    Alert.alert(
+      'Comming Sonn!',
+      'We`re hard at work on this feature, check back in the near future.',
+      [{ text: 'OK', onPress: () => console.log('User pressed OK') }]
+    )
+  }
   render() {
     return (
-      <TabBarIOS>
+      <TabBarIOS
+        barTintColor={globalStyles.BAR_COLOR}
+        tintColor={globalStyles.LINK_COLOR}
+        translucent={false}
+      >
         <TabBarIOS.Item
           systemIcon={'featured'}
           selected={this.state.tab === 'newsFeed'}
@@ -31,7 +47,7 @@ class HomeScreen extends Component {
         <TabBarIOS.Item
           systemIcon={'bookmarks'}
           selected={this.state.tab === 'bookmarks'}
-          onPre={() => this.setState({ tab: 'bookmarks' })}
+          onPress={() => this.setState({ tab: 'bookmarks' })}
         >
           <Text>Bookmarks</Text>
         </TabBarIOS.Item>
